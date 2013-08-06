af6705c3635bdb5d3dbf973802d25ef1fe2c7905
Pratik Shah <pratik@joshsoftware.com>
2013-08-06 22:20:54 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickIt::Application.config.secret_token = '613bd1045e98f6b9feca81ea2dbf08fc534234157cf8cfe4ddb400eb08b4efc1c9be45b018de023c7dfbda29dbc529e390bb865cfaf031dfc4e08e8f741b4a8b'
